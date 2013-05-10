29b7cddad904469f8e08f2578b269f3cf65e48fe
Michael J Salera <mikesalera@mac.com>
2013-05-10 09:10:12 -0700

    Updates to v 2.1.4 and version 26 - removed Hibernate version

--- BatesGrailsPlugin.groovy
+++ BatesGrailsPlugin.groovy
@@ -1,13 +1,12 @@
 class BatesGrailsPlugin {
     // the plugin version
-    def version = "0.25"
+    def version = "0.26"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
 
     // the other plugins this plugin depends on
-    def dependsOn = [mongodbMorphia: "0.8.2",
-        hibernate: "2.1.1"]
+    def dependsOn = [mongodbMorphia: "0.8.2"]
 //	executor: "0.3"]
 
     // resources that are excluded from plugin packaging
